3d6718a25193d527b8fcf26324f425a92f3a4c00
deMENSEN-JVD <jochem.vandenbussche@demensen.be>
2015-04-18 15:29:39 +0200

    Cleanup

--- themes/deMENSEN/views/dashboard/dashboard/index.php
+++ themes/deMENSEN/views/dashboard/dashboard/index.php
@@ -1,8 +1,5 @@
 <?php
 
-// Dirty fix - redirect - works
-//CController::redirect('/index.php?r=space/space&sguid=6c5523a2-0572-40cc-92f3-56b6eb6b9e9d');
-
 // Load 'space' module
 //Yii::app()->getModule('space');
 
@@ -24,59 +21,34 @@ $space=SpaceMembership::GetUserSpaces()[0];
             <br/>
         </div>
 
-        <?php if (isset($this->hideSidebar) && $this->hideSidebar) : ?>
-            <div class="col-md-10 layout-content-container">
-                <?php echo $content; ?>
-            </div>
-        <?php else: ?>
-            <div class="col-md-7 layout-content-container">
-                <?php
-                    $this->widget('application.modules_core.post.widgets.PostFormWidget', array(
-    'contentContainer' => $space,
-));
+        <div class="col-md-7 layout-content-container">
+            <?php
+                $this->widget('application.modules_core.post.widgets.PostFormWidget', array(
+                    'contentContainer' => $space,
+            ));
 
-$this->widget('application.modules_core.wall.widgets.StreamWidget', array(
-    'contentContainer' => $space,
-    'streamAction' => '//space/space/stream',
-    'messageStreamEmpty' => ($space->canWrite()) ?
-            Yii::t('SpaceModule.views_space_index', '<b>This space is still empty!</b><br>Start by posting something here...') :
-            Yii::t('SpaceModule.views_space_index', '<b>This space is still empty!</b>'),
-    'messageStreamEmptyCss' => ($space->canWrite()) ?
-            'placeholder-empty-stream' :
-            '',
-));
-                ?>
-            </div>
-            <div class="col-md-3 layout-sidebar-container">
-                <?php
+                $this->widget('application.modules_core.wall.widgets.StreamWidget', array(
+                    'contentContainer' => $space,
+                    'streamAction' => '//space/space/stream',
+                    'messageStreamEmpty' => ($space->canWrite()) ?
+                            Yii::t('SpaceModule.views_space_index', '<b>This space is still empty!</b><br>Start by posting something here...') :
+                            Yii::t('SpaceModule.views_space_index', '<b>This space is still empty!</b>'),
+                    'messageStreamEmptyCss' => ($space->canWrite()) ?
+                            'placeholder-empty-stream' :
+                            '',
+                ));
+            ?>
+        </div>
+        <div class="col-md-3 layout-sidebar-container">
+            <?php
                 $this->widget('application.modules_core.space.widgets.SpaceSidebarWidget', array(
                     'widgets' => array(
                         array('application.modules_core.activity.widgets.ActivityStreamWidget', array('contentContainer' => $space, 'streamAction' => '//space/space/stream'), array('sortOrder' => 100)),
                         array('application.modules_core.space.widgets.SpaceMemberWidget', array('space' => $space), array('sortOrder' => 200)),
                     )
                 ));
-                ?>
-            </div>
-        <?php endif; ?>
+            ?>
+        </div>
     </div>
 </div>
-
-<?php
-
-/*$this->widget('application.modules_core.post.widgets.PostFormWidget', array(
-    'contentContainer' => $space,
-));
-
-$this->widget('application.modules_core.wall.widgets.StreamWidget', array(
-    'contentContainer' => $space,
-    'streamAction' => '//space/space/stream',
-    'messageStreamEmpty' => ($space->canWrite()) ?
-            Yii::t('SpaceModule.views_space_index', '<b>This space is still empty!</b><br>Start by posting something here...') :
-            Yii::t('SpaceModule.views_space_index', '<b>This space is still empty!</b>'),
-    'messageStreamEmptyCss' => ($space->canWrite()) ?
-            'placeholder-empty-stream' :
-            '',
-));*/
-
-?>
 
